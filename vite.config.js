88afce59550db1a77e0ce1c44fca10131bb1fd5c
Shalim Sequeda <shalimsequesa@gmail.com>
2025-10-16 11:07:46 -0500

    Corrige configuración de Vercel para dominio raíz

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ✅ Configuración oficial y compatible con Vercel
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Carpeta que Vercel usará para desplegar
-  },
-  server: {
-    host: true,
+    outDir: "dist",
   },
 });
